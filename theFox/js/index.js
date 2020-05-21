6ade33e11a23df998403df3e8f5cacf6c4b4563c
NCT99 <chitao5799@gmail.com>
2020-05-21 23:21:28 +0700

    làm slide phần 1

--- theFox/js/index.js
+++ theFox/js/index.js
@@ -10,17 +10,37 @@
 	$('#ItemMenuSearch .iconSearch').click(function(){
 		$('#ItemMenuSearch').children('.itemMenuSearch').toggleClass('showItemMenuSearch');
 		if(isClickSearch==false){
-			$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-times"></i>');
+			$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-times" id="btnSearch"></i>');
 			isClickSearch=true;
 		}
 		else if(isClickSearch==true){
-			$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-search"></i>');
+			$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-search" id="btnSearch"></i>');
 			isClickSearch=false;
 		}
 		
 	});
+	window.onclick = function(event) {
+		// khi form tìm kiếm hiện lên và click chuột ra ngoài form tìm kiếm thì cũng đóng form
+			//	if (document.getElementsByClassName('showItemMenuSearch')[0]!==undefined && event.target !==$('#btnSearch path') && event.target!==$('#btnSearch') && event.target !==$('#ItemMenuSearch') && event.target!==$(".iconSearch") && event.target !==$('.itemMenuSearch') 
 
-	// $(":not(#ItemMenuSearch,.iconSearch,.itemMenuSearch)").click(function(){
-	// 		$('#ItemMenuSearch').children('.itemMenuSearch').removeClass('showItemMenuSearch');
-	// });
+	       if (document.getElementsByClassName('showItemMenuSearch')[0]!==undefined && event.target !==$('#btnSearch path') 
+	       	&& event.target.id !=='btnSearch' && event.target.id !=='ItemMenuSearch' && event.target.className !=="iconSearch"
+	       	 && event.target.className[0] !=='itemMenuSearch'&& event.target.nodeName !=='INPUT'&& event.target.nodeName !=='path'
+	       	 && event.target.className !=="input"&& event.target.className !=="itemMenuSearch showItemMenuSearch") 
+	        {
+	        //	alert('id là:'+event.target.id +"  class là:"+event.target.className+" node name"+event.target.nodeName);
+	            $('#ItemMenuSearch').children('.itemMenuSearch').toggleClass('showItemMenuSearch');
+	            if(isClickSearch==false){
+					$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-times" id="btnSearch"></i>');
+					isClickSearch=true;
+				}
+				else if(isClickSearch==true){
+					$('#ItemMenuSearch').children('.iconSearch').html('<i class="fas fa-search" id="btnSearch"></i>');
+					isClickSearch=false;
+				}
+	        }
+	    }
+		// $(":not(#ItemMenuSearch,.iconSearch,.itemMenuSearch)").click(function(){
+		// 		$('#ItemMenuSearch').children('.itemMenuSearch').removeClass('showItemMenuSearch');
+		// });
 // });
