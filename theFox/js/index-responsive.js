b9ec1c5f51df7cbce61e7bd5a51069fb77d144ce
NCT99 <chitao5799@gmail.com>
2021-01-10 22:30:06 +0700

    code for swipe screen hide show menu

--- theFox/js/index-responsive.js
+++ theFox/js/index-responsive.js
@@ -350,3 +350,68 @@
 		});
 			
 // });
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+		//alert("left swipe");
+		//nếu chưa hiện menu thì khi vuốt sang trái sẽ hiện menu
+		if(!document.querySelector("#menu-top.menuTopAppear")){
+			//set checked true cho checkbox để label của nó thành dấu X
+			document.querySelector('#menu-btn').checked=true;
+			
+			$('body').toggleClass('bodyToLeft');
+			$('#menu-top').toggleClass('menuTopAppear');
+		}
+        } else {
+            /* right swipe */
+		//alert("right swipe");
+		//nếu đã hiện menu thì vuốt sang phải sẽ ẩn menu
+		if(document.querySelector("#menu-top.menuTopAppear")){
+			//set checked false cho checkbox để label của nó thành dấu 3 gạch ngang
+			document.querySelector('#menu-btn').checked=false;
+			
+			$('body').toggleClass('bodyToLeft');
+			$('#menu-top').toggleClass('menuTopAppear');
+		}
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+		//alert("up swipe");
+        } else { 
+            /* down swipe */
+	//	alert("down swipe");
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
